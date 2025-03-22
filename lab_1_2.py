f2b9b41ada54e5b3e49a81078d08b7f9e55e20a1
Alina <terehovalinas@yandex.ru>
2025-03-22 16:49:43 +0700

    Задание 1.2

--- lab_1_2.py
+++ lab_1_2.py
@@ -8,6 +8,6 @@ numbers = [num1, num2, num3]
 in_interval = [num for num in numbers if 1<= num <=50]
 
 if in_interval:
-    print("Числа, входящие в интервал:", ", ".join(map(str, in_range)))
+    print("Числа, входящие в интервал:", ", ".join(map(str, in_interval)))
 else:
     print("Нет чисел, входящих в интервал.")
