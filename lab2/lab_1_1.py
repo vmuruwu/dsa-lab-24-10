475bb33772e5501d6c45aa9ec781221f9f8bac7f
Алина Терехова <terehovalinas@yandex.ru>
2025-04-03 15:22:24 +0700

    Задание 1

--- lab2/lab_1_1.py
+++ lab2/lab_1_1.py
@@ -1,7 +1,18 @@
 ##Считать с клавиатуры три произвольных числа, найти минимальное среди них и вывести на экран.
 
-a = int(input('Введите число 1: '))
-b  = int(input('Введите число 2: '))
-c = int(input('Введите число 3: '))
 
-print('Минимальное число: ', min(a, b, c))
+number_1 = float(input("Введите первое число: "))
+number_2 = float(input("Введите второе число: "))
+number_3 = float(input("Введите третье число: "))
+
+min_number = number_1
+
+if number_2 < min_number:
+    min_number = number_2
+if number_3 < min_number:
+    min_number = number_3
+
+print("Минимальное число:", min_number)
+
+
+
