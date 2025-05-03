25ee2d110899ff2b1024a3f4d69e5ab9f3c55c9d
Vtyurina Natalya <160294758+vmuruwu@users.noreply.github.com>
2025-05-03 15:06:48 +0700

    Delete lab3 directory

--- lab3/request_delete.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-response = requests.delete("http://127.0.0.1:5000/number/")
-print(response.json())
--- lab3/request_post.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/number/"
-data = {"jsonParam": 7}  # Задаём число
-
-response = requests.post(url, json=data)
-print(response.json())
